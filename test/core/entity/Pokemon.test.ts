ca55c8a0f90809269161ed07d8c1b7bb36ca3e2a
azkahi <azkaimtiyaz@gmail.com>
2023-01-08 18:24:40 +0700

    Add pokemon unit test in regard to added methods

--- test/core/entity/Pokemon.test.ts
+++ test/core/entity/Pokemon.test.ts
@@ -13,4 +13,40 @@ describe('Pokemon entity', () => {
     expect(pokemon.stats[0].base).toBe(6)
     expect(pokemon.prevBerry).toBe(undefined)
   })
+
+  test('Should change pokemon weight', async function () {
+    const sprites = ['sprite_url']
+    const stats = [{ label: 'Speed', base: 6 }]
+    const pokemon = new Pokemon('Test Pokemon', 3, sprites, stats, undefined)
+
+    pokemon.changePokemonWeight(3)
+
+    expect(pokemon.weight).toBe(6)
+
+    pokemon.changePokemonWeight(-10)
+
+    expect(pokemon.weight).toBe(1)
+  })
+
+  test('Should check pokemon berry to be not okay', async function () {
+    const sprites = ['sprite_url']
+    const stats = [{ label: 'Speed', base: 6 }]
+    const berry = new Berry('Test Berry', 'hard', 'sprite_url')
+    const pokemon = new Pokemon('Test Pokemon', 3, sprites, stats, berry)
+
+    const nextBerry = new Berry('Next Berry', 'hard', 'sprite_url')
+
+    expect(pokemon.checkBerryOkay(nextBerry)).toBe(false)
+  })
+
+  test('Should check pokemon berry to be okay', async function () {
+    const sprites = ['sprite_url']
+    const stats = [{ label: 'Speed', base: 6 }]
+    const berry = new Berry('Test Berry', 'others', 'sprite_url')
+    const pokemon = new Pokemon('Test Pokemon', 3, sprites, stats, berry)
+
+    const nextBerry = new Berry('Next Berry', 'hard', 'sprite_url')
+
+    expect(pokemon.checkBerryOkay(nextBerry)).toBe(true)
+  })
 })
