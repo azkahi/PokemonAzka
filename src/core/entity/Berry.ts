4af397985834f28bd18cc34cc4b3a385063f5088
azkahi <azkaimtiyaz@gmail.com>
2023-01-08 10:27:14 +0700

    Implement Berry Class

---
+++ src/core/entity/Berry.ts
@@ -0,0 +1,34 @@
+export default class Berry {
+  name: string
+  berryType: BerryType
+  weightInKg: number
+
+  constructor(name: string, berryType: string) {
+    this.name = name
+    this.berryType = berryType as unknown as BerryType
+    this.weightInKg =
+      BerryTypeWeightInKg[
+        Object.keys(BerryType)[
+          Object.values(BerryType).indexOf(this.berryType)
+        ] as keyof typeof BerryTypeWeightInKg
+      ]
+  }
+}
+
+export enum BerryType {
+  VerySoft = 'very-soft',
+  Soft = 'soft',
+  Hard = 'hard',
+  VeryHard = 'very-hard',
+  SuperHard = 'super-hard',
+  Others = 'others',
+}
+
+export enum BerryTypeWeightInKg {
+  VerySoft = 2,
+  Soft = 3,
+  Hard = 5,
+  VeryHard = 8,
+  SuperHard = 10,
+  Others = 1,
+}
