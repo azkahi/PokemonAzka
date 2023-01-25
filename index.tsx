9678139985e1419c5b7adef6b41a5d741f3924ea
azkahi <azkaimtiyaz@gmail.com>
2023-01-25 07:41:04 +0700

    Add recoil

--- index.tsx
+++ index.tsx
@@ -2,20 +2,35 @@ import 'react-native-gesture-handler'
 import 'react-native-reanimated'
 
 import { registerRootComponent } from 'expo'
+import React from 'react'
+import { View, Text } from 'react-native'
+import { RecoilRoot } from 'recoil'
 
 import { AppLoading } from './src/infra/components'
 import { Navigation } from './src/infra/navigation'
 import { SafeAreaProvider } from './src/infra/providers'
 import { ColorSchemeProvider } from './src/infra/providers/ColorSchemeProvider'
 
+const LoadingSuspense = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Loading</Text>
+    </View>
+  )
+}
+
 const App = (): JSX.Element => {
   return (
     <SafeAreaProvider>
-      <AppLoading>
-        <ColorSchemeProvider>
-          <Navigation />
-        </ColorSchemeProvider>
-      </AppLoading>
+      <React.Suspense fallback={<LoadingSuspense />}>
+        <RecoilRoot>
+          <AppLoading>
+            <ColorSchemeProvider>
+              <Navigation />
+            </ColorSchemeProvider>
+          </AppLoading>
+        </RecoilRoot>
+      </React.Suspense>
     </SafeAreaProvider>
   )
 }
