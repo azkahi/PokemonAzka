9a9988b5ad87589b275421554d87cdc35aa01eba
azkahi <azkaimtiyaz@gmail.com>
2023-01-26 12:46:41 +0700

    Config add

--- app.config.ts
+++ app.config.ts
@@ -4,6 +4,10 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
   return {
     ...config,
     extra: {
+      eas: {
+        projectId: '1448cbe6-2fa9-48fe-a042-44e17e88c6e0',
+      },
+      baseUrl: 'https://pokeapi.co/api/v2',
       universalLinks: [],
     },
   }
